fd5966898a9a695fd093ae0a19f0a5d32c885ccb
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2024-11-24 03:12:57 +0530

    Initial Project Structure Commit

---
+++ src/main/java/org/esdpracticals/academicerp/AcademicErpApplication.java
@@ -0,0 +1,13 @@
+package org.esdpracticals.academicerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/esdpracticals/academicerp/AcademicErpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esdpracticals.academicerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
