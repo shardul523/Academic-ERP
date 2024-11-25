20c8a81c6440b6f97e54ff96dd6d3ac17b8cfbdd
Shardul Sisodiya <sisodiyashardul523@gmail.com>
2024-11-26 03:17:59 +0530

    [Fix] Applied the following fixes - Removed unnecessary import from Course.java

--- src/main/java/org/esdpracticals/academicerp/entity/Course.java
+++ src/main/java/org/esdpracticals/academicerp/entity/Course.java
@@ -2,7 +2,6 @@ package org.esdpracticals.academicerp.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
