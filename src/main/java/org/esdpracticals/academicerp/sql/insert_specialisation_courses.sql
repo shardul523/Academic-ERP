88f7ac262fee637323fbf66f364ca271d28eb9fe
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2024-11-26 05:53:47 +0530

    [Test] - Created SQL scripts for inserting dummy data for - Course Schedule - Specialisation Courses - Specialisations

---
+++ src/main/java/org/esdpracticals/academicerp/sql/insert_specialisation_courses.sql
@@ -0,0 +1,14 @@
+INSERT INTO Specialisation_Course (specialisation_id, course_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (2, 5),
+    (2, 6),
+    (2, 8),
+    (2, 7),
+    (3, 9),
+    (3, 10),
+    (3, 11),
+    (3, 12);
